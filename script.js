f84dc22d68cbfa02c6b91fba925727d221c32257
bhin0005 <59697523+bhin0005@users.noreply.github.com>
2021-02-27 00:42:22 -0500

    ASSINGMENT FINISH

---
+++ script.js
@@ -0,0 +1,301 @@
+
+    let e1 = document.getElementById('e1');
+    e1.innerHTML = '&#8986;';
+
+    let e2 = document.getElementById('e2');
+    e2.innerHTML = '&#129409;';
+
+    let e3 = document.getElementById('e3');
+    e3.innerHTML = '&#8987;';
+
+    let e4 = document.getElementById('e4');
+    e4.innerHTML = '&#9193;';
+
+    let e5 = document.getElementById('e5');
+    e5.innerHTML = '&#9194;';
+
+    let e6 = document.getElementById('e6');
+    e6.innerHTML = '&#9195;';
+
+    let e7 = document.getElementById('e7');
+    e7.innerHTML = '&#9196;';
+
+    let e8 = document.getElementById('e8');
+    e8.innerHTML = '&#9197;';
+
+    let e9 = document.getElementById('e9');
+    e9.innerHTML = '&#9198;';
+
+    let e10 = document.getElementById('e10');
+    e10.innerHTML = '&#9199;';
+
+    
+    let e11 = document.getElementById('e11');
+    e11.innerHTML = '&#9200;';
+
+    let e12 = document.getElementById('e12');
+    e12.innerHTML = '&#9201;';
+
+    let e13 = document.getElementById('e13');
+    e13.innerHTML = '&#9202;';
+
+    let e14 = document.getElementById('e14');
+    e14.innerHTML = '&#9203;';
+
+    let e15 = document.getElementById('e15');
+    e15.innerHTML = '&#9204;';
+
+    let e16 = document.getElementById('e16');
+    e16.innerHTML = '&#9205;';
+
+    let e17 = document.getElementById('e17');
+    e17.innerHTML = '&#9206;';
+
+    let e18 = document.getElementById('e18');
+    e18.innerHTML = '&#9207;';
+
+    let e19 = document.getElementById('e19');
+    e19.innerHTML = '&#9208;';
+
+    let e20 = document.getElementById('e20');
+    e20.innerHTML = '&#9209;';
+
+    let e21 = document.getElementById('e21');
+    e21.innerHTML = '&#9210;';
+
+    let e22 = document.getElementById('e22');
+    e22.innerHTML = '&#9410;';
+
+    let e23 = document.getElementById('e23');
+    e23.innerHTML = '&#9748;';
+
+    let e24 = document.getElementById('e24');
+    e24.innerHTML = '&#9749;';
+
+    let e25 = document.getElementById('e25');
+    e25.innerHTML = '&#9757;';
+
+    let e26 = document.getElementById('e26');
+    e26.innerHTML = '&#9800;';
+
+    let e27 = document.getElementById('e27');
+    e27.innerHTML = '&#9801;';
+
+    let e28 = document.getElementById('e28');
+    e28.innerHTML = '&#9802;';
+
+    let e29 = document.getElementById('e29');
+    e29.innerHTML = '&#9803;';
+
+    let e30 = document.getElementById('e30');
+    e30.innerHTML = '&#9804;';
+
+    let e31 = document.getElementById('e31');
+    e31.innerHTML = '&#8805;';
+
+    let e32 = document.getElementById('e32');
+    e32.innerHTML = '&#9806;';
+
+    let e33 = document.getElementById('e33');
+    e33.innerHTML = '&#9807;';
+
+    let e34 = document.getElementById('e34');
+    e34.innerHTML = '&#9808;';
+
+    let e35 = document.getElementById('e35');
+    e35.innerHTML = '&#9809;';
+
+    let e36 = document.getElementById('e36');
+    e36.innerHTML = '&#9810;';
+
+    let e37 = document.getElementById('e37');
+    e37.innerHTML = '&#9811;';
+
+    let e38 = document.getElementById('e38');
+    e38.innerHTML = '&#127872;';
+
+    let e39 = document.getElementById('e39');
+    e39.innerHTML = '&#9855;';
+
+    let e40 = document.getElementById('e40');
+    e40.innerHTML = '&#9875;';
+
+    let e41 = document.getElementById('e41');
+    e41.innerHTML = '&#8889;';
+
+    let e42 = document.getElementById('e42');
+    e42.innerHTML = '&#9898;';
+
+    let e43 = document.getElementById('e43');
+    e43.innerHTML = '&#9899;';
+
+    let e44 = document.getElementById('e44');
+    e44.innerHTML = '&#9917;';
+
+    let e45 = document.getElementById('e45');
+    e45.innerHTML = '&#9918;';
+
+    let e46 = document.getElementById('e46');
+    e46.innerHTML = '&#9924;';
+
+    let e47 = document.getElementById('e47');
+    e47.innerHTML = '&#9925;';
+
+    let e48 = document.getElementById('e48');
+    e48.innerHTML = '&#9934;';
+
+    let e49 = document.getElementById('e49');
+    e49.innerHTML = '&#9935;';
+
+    let e50 = document.getElementById('e50');
+    e50.innerHTML = '&#9937;';
+
+    let e51 = document.getElementById('e51');
+    e51.innerHTML = '&#9939;';
+
+    let e52 = document.getElementById('e52');
+    e52.innerHTML = '&#9940;';
+
+    let e53 = document.getElementById('e53');
+    e53.innerHTML = '&#9961;';
+
+    let e54 = document.getElementById('e54');
+    e54.innerHTML = '&#9962;';
+
+    let e55 = document.getElementById('e55');
+    e55.innerHTML = '&#9968;';
+
+    let e56 = document.getElementById('e56');
+    e56.innerHTML = '&#9969;';
+
+    let e57 = document.getElementById('e57');
+    e57.innerHTML = '&#9970;';
+
+    let e58 = document.getElementById('e58');
+    e58.innerHTML = '&#9971;';
+
+    let e59 = document.getElementById('e59');
+    e59.innerHTML = '&#9972;';
+
+    let e60 = document.getElementById('e60');
+    e60.innerHTML = '&#9973;';
+
+    let e61 = document.getElementById('e61');
+    e61.innerHTML = '&#9975;';
+
+    let e62 = document.getElementById('e62');
+    e62.innerHTML = '&#9976;';
+
+    let e63 = document.getElementById('e63');
+    e63.innerHTML = '&#9977;';
+
+    let e64 = document.getElementById('e64');
+    e64.innerHTML = '&#9978;';
+
+    let e65 = document.getElementById('e65');
+    e65.innerHTML = '&#9981;';
+
+    let e66 = document.getElementById('e66');
+    e66.innerHTML = '&#9986;';
+
+    let e67 = document.getElementById('e67');
+    e67.innerHTML = '&#9989;';
+
+    let e68 = document.getElementById('e68');
+    e68.innerHTML = '&#9992;';
+
+    let e69 = document.getElementById('e69');
+    e69.innerHTML = '&#9993;';
+
+    let e70 = document.getElementById('e70');
+    e70.innerHTML = '&#9994;';
+
+    let e71 = document.getElementById('e71');
+    e71.innerHTML = '&#9995;';
+
+    let e72 = document.getElementById('e72');
+    e72.innerHTML = '&#9996;';
+
+    let e73 = document.getElementById('e73');
+    e73.innerHTML = '&#9997;';
+
+    let e74 = document.getElementById('e74');
+    e74.innerHTML = '&#9999;';
+
+    let e75 = document.getElementById('e75');
+    e75.innerHTML = '&#10002;';
+
+    let e76 = document.getElementById('e76');
+    e76.innerHTML = '&#10004;';
+
+    let e77 = document.getElementById('e77');
+    e77.innerHTML = '&#10006;';
+
+    let e78 = document.getElementById('e78');
+    e78.innerHTML = '&#10013;';
+
+    let e79 = document.getElementById('e79');
+    e79.innerHTML = '&#10017;';
+
+    let e80 = document.getElementById('e80');
+    e80.innerHTML = '&#10024;';
+
+    let e81 = document.getElementById('e81');
+    e81.innerHTML = '&#10035;';
+
+    let e82 = document.getElementById('e82');
+    e82.innerHTML = '&#10036;';
+
+    let e83 = document.getElementById('e83');
+    e83.innerHTML = '&#10052;';
+
+    let e84 = document.getElementById('e84');
+    e84.innerHTML = '&#10055;';
+
+    let e85 = document.getElementById('e85');
+    e85.innerHTML = '&#10060;';
+
+    let e86 = document.getElementById('e86');
+    e86.innerHTML = '&#127853;';
+
+    let e87 = document.getElementById('e87');
+    e87.innerHTML = '&#10067;';
+
+    let e88 = document.getElementById('e88');
+    e88.innerHTML = '&#10068;';
+
+    let e89 = document.getElementById('e89');
+    e89.innerHTML = '&#10069;';
+
+    let e90 = document.getElementById('e90');
+    e90.innerHTML = '&#10071;';
+
+    let e91 = document.getElementById('e91');
+    e91.innerHTML = '&#10083;';
+
+    let e92 = document.getElementById('e92');
+    e92.innerHTML = '&#10084;';
+
+    let e93 = document.getElementById('e93');
+    e93.innerHTML = '&#10133;';
+
+    let e94 = document.getElementById('e94');
+    e94.innerHTML = '&#10134;';
+
+    let e95 = document.getElementById('e95');
+    e95.innerHTML = '&#10135;';
+
+    let e96 = document.getElementById('e96');
+    e96.innerHTML = '&#10145;';
+
+    let e97 = document.getElementById('e97');
+    e97.innerHTML = '&#10160;';
+
+    let e98 = document.getElementById('e98');
+    e98.innerHTML = '&#10175;';
+
+    let e99 = document.getElementById('e99');
+    e99.innerHTML = '&#10548;';
+
+    let e100 = document.getElementById('e100');
+    e100.innerHTML = '&#10549;';
